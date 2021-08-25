84cfd8b19868a00c24418c13c5b21751d80e4b90
coffmanjrp <coffmanjrp@gmail.com>
2021-08-25 11:08:41 -0700

    HTML and styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,17 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <button className="toggle">Dark mode</button>
+      <div className="clock-container">
+        <div className="clock">
+          <div className="needle hour"></div>
+          <div className="needle minute"></div>
+          <div className="needle second"></div>
+          <div className="center-point"></div>
+        </div>
+        <div className="time"></div>
+        <div className="date"></div>
+      </div>
     </div>
   );
 }
